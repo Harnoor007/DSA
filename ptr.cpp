640061d691c5e4be322fcac9cfe9123ccc0908c2
Harnoor <harnoorsingh836@gmail.com>
2024-01-28 17:51:33 +0530

    Jan 282

--- ptr.cpp
+++ ptr.cpp
@@ -4,13 +4,17 @@ int main(){
     int i = 5;
     int* ptr = &i;
     int** ptr2 = &ptr;
-    cout<< "&i " <<&i <<endl; //addr of i
-    cout<< "ptr " <<ptr <<endl; //stored addr at ptr
+
     cout<< "*ptr " <<*ptr <<endl; //derefrence of addr stored at ptr
     cout<< "**ptr2 " <<**ptr2 <<endl; //ptr2 -> ptr -> value printed
+
+    cout<< "&i " <<&i <<endl; //addr of i
+    cout<< "ptr " <<ptr <<endl; //stored addr at ptr
+    cout<< "*ptr2 " <<*ptr2 <<endl; // ptr2 addr ch jo variable aa, othe jo value pyi aa ie i da addr
+
     cout<< "&ptr " <<&ptr <<endl; //addr of ptr
-    cout<< "ptr2 " <<ptr2 <<endl; //addr stored at ptr2
-    cout<< "*ptr2 " <<*ptr2 <<endl; // ptr2 addr ch jo variable aa, othe jo value pyi aa
+    cout<< "ptr2 " <<ptr2 <<endl; //addr stored at ptr2, addr of ptr
+    
     
     return 0;
 }
